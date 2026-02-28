738b1351d2ccf39b11c367b50624649f0b43306b
foyoodo <foyoodo@gmail.com>
2026-02-28 14:34:36 +0800

    Update focus.lua

--- hammerspoon/.hammerspoon/focus.lua
+++ hammerspoon/.hammerspoon/focus.lua
@@ -1,19 +1,21 @@
-function focus(bundleId)
+function launchOrFocusByBundleID(bundleId)
     return function()
         hs.application.launchOrFocusByBundleID(bundleId)
     end
 end
 
-local terminal = "com.googlecode.iterm2" -- "com.apple.Terminal"
+-- "com.apple.Terminal"
+-- "com.googlecode.iterm2"
+local terminal = "com.mitchellh.ghostty"
 
-hs.hotkey.bind({"alt"}, "1", focus("com.apple.finder"))
-hs.hotkey.bind({"alt"}, "2", focus("net.imput.helium"))
-hs.hotkey.bind({"alt"}, "3", focus("com.apple.Safari"))
-hs.hotkey.bind({"alt"}, "E", focus("com.sublimetext.4"))
-hs.hotkey.bind({"alt"}, "T", focus(terminal))
-hs.hotkey.bind({"alt"}, "F", focus("org.mozilla.librewolf"))
-hs.hotkey.bind({"alt"}, "X", focus("com.apple.dt.Xcode"))
-hs.hotkey.bind({"alt"}, "Z", focus("dev.zed.Zed"))
-hs.hotkey.bind({"alt"}, "C", focus("com.microsoft.VSCode"))
-hs.hotkey.bind({"alt"}, "V", focus("com.sublimemerge"))
-hs.hotkey.bind({"alt"}, "M", focus("app.cyan.markedit"))
+hs.hotkey.bind({"alt"}, "1", launchOrFocusByBundleID("com.apple.finder"))
+hs.hotkey.bind({"alt"}, "2", launchOrFocusByBundleID("net.imput.helium"))
+hs.hotkey.bind({"alt"}, "3", launchOrFocusByBundleID("com.apple.Safari"))
+hs.hotkey.bind({"alt"}, "E", launchOrFocusByBundleID("com.sublimetext.4"))
+hs.hotkey.bind({"alt"}, "T", launchOrFocusByBundleID(terminal))
+hs.hotkey.bind({"alt"}, "F", launchOrFocusByBundleID("org.mozilla.librewolf"))
+hs.hotkey.bind({"alt"}, "X", launchOrFocusByBundleID("com.apple.dt.Xcode"))
+hs.hotkey.bind({"alt"}, "Z", launchOrFocusByBundleID("dev.zed.Zed"))
+hs.hotkey.bind({"alt"}, "C", launchOrFocusByBundleID("com.microsoft.VSCode"))
+hs.hotkey.bind({"alt"}, "V", launchOrFocusByBundleID("com.sublimemerge"))
+hs.hotkey.bind({"alt"}, "M", launchOrFocusByBundleID("app.cyan.markedit"))
